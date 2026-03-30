d2ccb3030c6243cfad9b071d7fd27193246ba124
Nikhil Kumar <nikhilnikumar@gmail.com>
2026-03-30 22:19:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -101,12 +101,12 @@ with open("columns.json","w") as f:
 
 # train test split
 
-X_trian,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
 
 # train model
 
 model=LinearRegression()
-model.fit(X_trian,Y_train)
+model.fit(X_train,Y_train)
 
 # Evaluate
 
